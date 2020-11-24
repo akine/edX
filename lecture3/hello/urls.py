7fab8818d1258fda711df723a053130689de1570
akine <weezer.and.you@gmail.com>
2020-11-24 09:11:05 +0900

    done with helloapp

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("akine", views.akine, name="akine"),
+    path("david", views.david, name="david")
 ]
